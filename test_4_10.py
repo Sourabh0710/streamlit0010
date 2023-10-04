eb0f984dc23d8b4f7bc5a2fca30159427f276d72
Sourabh Alimchandani <105855030+Sourabh0710@users.noreply.github.com>
2023-10-04 16:41:54 +0530

    Add files via upload

---
+++ test_4_10.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from bs4 import BeautifulSoup
+import requests
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+# Load the T5 model and tokenizer
+model_name = "t5-large"
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+# Streamlit app layout
+st.title("Web News Summarizer")
+supplier_link = st.text_input("Enter the supplier's web link:")
+
+if st.button("Fetch and Summarize"):
+    try:
+        # Function to fetch and summarize the article
+        def fetch_and_summarize(link):
+            # Fetch the web page content
+            response = requests.get(link)
+            response.raise_for_status()  # Raise an HTTPError for bad responses
+
+            # Parse the web page using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Extract article text (modify as per the website's structure)
+            article_text = ""
+            for paragraph in soup.find_all('p'):
+                article_text += paragraph.text + "\n"
+
+            # Check if article text is empty
+            if not article_text.strip():
+                st.warning("No article text found on the web page.")
+                return
+
+            # Summarize the article using the T5 model
+            input_text = "summarize: " + article_text
+            input_ids = tokenizer.encode(input_text, return_tensors="pt", max_length=1024, truncation=True)
+            summary_ids = model.generate(input_ids, max_length=150, min_length=30, length_penalty=2.0, num_beams=4,
+                                         early_stopping=True)
+            summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+            # Display the summary
+            st.subheader("Summary:")
+            st.write(summary)
+
+        # Call the fetch_and_summarize function with the user-provided link
+        if supplier_link:
+            fetch_and_summarize(supplier_link)
+        else:
+            st.warning("Please enter a valid web link.")
+
+    except requests.exceptions.RequestException as req_err:
+        st.error(f"Failed to retrieve the web page. Error: {str(req_err)}")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+
+# Add a check for Streamlit
+if __name__ == '__main__':
+    st.write("App is running.")
